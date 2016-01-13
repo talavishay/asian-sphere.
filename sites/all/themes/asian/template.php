ba7be423fcd4450e4b569b10c8e1e9600e311c44
user <user@debian>
2016-01-13 12:24:35 +0200

    pre sync

--- sites/all/themes/asian/template.php
+++ sites/all/themes/asian/template.php
@@ -1,13 +1,17 @@
 <?php
 
 function asian_theme_preprocess_calendar_mini(&$vars,$name){
-	//~ dpm($vars);
-	
-	
-	$vars["test"] = "ZZZZZ";
+	//format header day names
+	foreach($vars["day_names"] as $k => $v){
+		$vars["day_names"][$k]["data"] = explode(" ", $v["class"])[1];
+	};
+	//dialog for event info
+	drupal_add_library('system', 'ui.dialog');
+	drupal_add_library('system', 'ui.tooltip');
 }
-
-
+function asian_theme_preprocess_calendar_datebox(&$vars){
+		//~ dpm($vars);
+}
 function asian_theme_calendar_empty_day($vars) {
 	$curday = $vars['curday'];
 	$view = $vars['view'];
@@ -19,7 +23,33 @@ function asian_theme_calendar_empty_day($vars) {
 		return '<div class="calendar-dayview-empty">' . t('Empty day') . '</div>';
 	  }
 	} else {
-	return '';
+		return '';
 	}
 }
+function asian_theme_preprocess_image(&$variables) {        
+	unset($variables['width']);
+	unset($variables['height']);
+}
+/*function asian_theme_preprocess_node(&$vars) {
+  if($vars['view_mode'] == 'teaser' && $vars['type'] == "gallery") {
+	  //~ $ni = array_slice($vars['content']['field_gallery_img']['#items'], 3);
+//~ $vars['content']['field_gallery_img']['#items'] = 
+    
+	  
+	  //~ dpm($vars['content']['field_gallery_img']['#items']);
+  }
+}*/
+function asian_theme_preprocess_field__field_gallery_img(&$vars) { 
+	$vars['items'] = array_slice($vars['items'], -3);
+	return $vars;
+}
+function asian_theme_preprocess_field(&$vars) { 
+	if($vars['element']['#field_name'] === "field_gallery_img"){
+		$function = 'asian_theme_preprocess_field__'. $vars['element']['#field_name'];
+		if(function_exists($function)) {
+			$vars = $function($vars);
+		}
+	}
+}
+
 ?>
